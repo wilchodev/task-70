14cbb4db8ed60e892af62792d651ef1d986def43
wilcho <vilislav91@gmail.com>
2022-01-11 16:40:36 +0200

    add scale

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,9 @@
-import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
+  const image = document.querySelector(".image");
+  image.addEventListener("click", function (event) {
+      image.classList.add('active');
   });
 });
